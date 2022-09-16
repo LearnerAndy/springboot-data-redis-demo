aa5616b45360323f89e0aef281f64663f4606e45
Learner_Andy <Learner_Andy@163.com>
2022-09-16 18:07:58 +0800

    v1.0_环境搭建

---
+++ src/main/java/com/xxxx/redis/App.java
@@ -0,0 +1,13 @@
+package com.xxxx.redis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
