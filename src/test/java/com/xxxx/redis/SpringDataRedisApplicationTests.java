4c53abbddfc72dc24f20cf19e5c8ff415d12c47d
Learner_Andy <Learner_Andy@163.com>
2022-09-17 14:58:51 +0800

    v1.0_自定义模板解决序列化问题

--- src/test/java/com/xxxx/redis/SpringDataRedisApplicationTests.java
+++ src/test/java/com/xxxx/redis/SpringDataRedisApplicationTests.java
@@ -1,5 +1,6 @@
 package com.xxxx.redis;
 
+import com.xxxx.redis.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,13 +8,18 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
+import javax.annotation.Resource;
+
 @SpringBootTest(classes = SpringDataRedisApplication.class)
 public class SpringDataRedisApplicationTests {
     @Autowired
-    private RedisTemplate<Object, Object> redisTemplate;
+    private RedisTemplate redisTemplate;
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    /**
+     * 测试项目搭建
+     */
     @Test
     public void initConn() {
         ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
@@ -25,9 +31,27 @@ public class SpringDataRedisApplicationTests {
         System.out.println(value.get("username"));//null
         System.out.println(ops.get("username"));//lisi
     }
+
+    /**
+     * 测试项目搭建
+     */
     @Test
     public void test01(){
         redisTemplate.opsForValue().set("cc","cc");
         System.out.println(redisTemplate.opsForValue().get("cc"));
+        System.out.println(redisTemplate.opsForValue().get("username"));
+    }
+
+    /**
+     * 测试序列化
+     */
+    @Test
+    public void test02(){
+        User user = new User();
+        user.setId(20);
+        user.setUserName("admin");
+        user.setPassword("root");
+        redisTemplate.opsForValue().set("user::id::200",user);
+        System.out.println(redisTemplate.opsForValue().get("user::id::200"));
     }
 }
